8309016dfe4039744b8c1a8f0dc40948e2083dbb
Chirag Kottary <ckottary18@gmail.com>
2025-09-18 12:21:25 +0530

    update configuration for environment variables

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const config = {
     NODEJS_API_BASE_URL: process.env.NODEJS_API_BASE_URL || 'http://pub400.com:3012/',
 
     // Order Service Configuration
-    ORDER_SERVICE_TOKEN: process.env.ORDER_SERVICE_TOKEN || 'eyJhbGciOiJSU0EiLCJ0eXAiOiJKV1QifQ==.eyJzdWIiOiJlZ3RfZGV2QGVndGVzdC1hZGJlZ3QwMDAwIiwicmVmcmVzaFRva2VuIjoiakcvWHM3d3owSjZSd1h0NXhmamhndz09IiwiZXhwIjoxNzYzMjE2MjQyODM3LCJhcGlLZXkiOnRydWUsInJvbGVzIjpbXSwibWV0YURhdGEiOnt9fQ==.X5GCuP+b1MRlJfNKDANLfbJp9yOHgn9crwSAUQMXj2qSI8A4u1tlqcWyj4M6THUUApQ/kv6SiMHOqziokZ303mgnhbOqFrP1UlQcv72e6McQ+z+00hQLoJntm3eBkQAXvYp2GQpJpFoxi8BLhzQpQgODFuom4kvaYEa+oQ1oroM3LAhCrAjHJuOlDLu/TNErLJe+u/ve4wpNcqnZziiyIWaCEwFb6km8p+tvqrJSt3Ci5/A6A8tAu20toh3+fNuZr2A+A3sxc61y+4bw6xKpBYJCU5wwfT99gRBEW2zsN4Y/tIt8AauXvTEEs37195Cpvb36snIRQM2WlPGLRNBCjg==',
+    ORDER_SERVICE_TOKEN: process.env.ORDER_SERVICE_TOKEN ,
 
     // Server Configuration
     SERVER_NAME: 'BuildMate Invoice & Order MCP Server',
